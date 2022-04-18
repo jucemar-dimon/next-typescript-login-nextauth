967b661a39416161177a94bee9c8547d4a55e4a8
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-04-18 10:44:33 -0300

    Adiciona a lib nookies e salva tokens nos cokkies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 import Router from "next/router";
+import { setCookie } from "nookies";
 import { api } from "../services/api";
 
 type SignInCredentials = {
@@ -35,9 +36,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
         email,
         password,
       });
-
       const { token, refreshToken, permissions, roles } = response.data;
       setUser({ email, permissions, roles });
+      setCookie(undefined, "next-typescript-login-nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+      setCookie(
+        undefined,
+        "next-typescript-login-nextauth.refreshToken",
+        refreshToken,
+        { maxAge: 60 * 60 * 24 * 30, path: "/" }
+      );
       Router.push("/dashboard");
     } catch (error) {
       console.log("Erro ao fazer login", error);
