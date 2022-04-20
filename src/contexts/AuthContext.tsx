845803ad0e3715ec923161743982e6b4d3f8fabc
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-04-20 11:29:37 -0300

    Adiciona token no header de todas requests à api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 import { api } from "../services/api";
 
 type SignInCredentials = {
@@ -30,6 +30,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "next-typescript-login-nextauth.token": token } = parseCookies();
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", {
@@ -48,6 +58,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         refreshToken,
         { maxAge: 60 * 60 * 24 * 30, path: "/" }
       );
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
       Router.push("/dashboard");
     } catch (error) {
       console.log("Erro ao fazer login", error);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
 
+const cookies = parseCookies();
+console.log('cookies', cookies);
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${cookies["next-typescript-login-nextauth.token"]}`
+
+  }
 })
